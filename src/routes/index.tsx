4a0c0ec2cade96aba01623dd6693ddfbce88b79f
Luiz Victorino <hnluizz@gmail.com>
2026-08-20 10:41:26 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -301,7 +301,7 @@ function Index() {
                           {c.tipo === "ribbon" ? "Ribbon" : "Etiqueta"}
                         </Badge>
                       </TableCell>
-                    {/*   <TableCell className="text-xs">
+                      <TableCell className="text-xs">
                         {c.semLote ? (
                           <span className="text-muted-foreground">Sem lote informado</span>
                         ) : (
@@ -311,10 +311,10 @@ function Index() {
                       <TableCell className="text-xs">
                         {c.lotes.map((l, i) => (
                           <div key={i}>{l.qLote}</div>
-                        ))} */}
+                        ))}
 
 
-                      <TableCell className="text-xs">
+{/*                       <TableCell className="text-xs">
                         {c.lotes.length === 0 ? (
                           <span className="text-muted-foreground">
                             Sem lote informado
@@ -334,7 +334,7 @@ function Index() {
                             .filter((l) => l.nLote.trim() !== "")
                             .map((l, i) => <div key={i}>{l.qLote}</div>)
                         )}
-                      </TableCell>
+                      </TableCell> */}
 
 
                       { /*----------------------*/}
